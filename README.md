93fc609a4c6fa947c6eff2ff22d6e3bada276964
CuriosityByte <189885948+Cur10s1tyByt3@users.noreply.github.com>
2025-02-09 08:05:02 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,12 @@
+# EasyPeasy
+
+Mirror repository for EasyPeasy in GitHub (Original GitLab repo: https://gitlab.com/snuggy/easypeasy), adapting *Allen Carr's EasyWay to Stop Smoking* for porn addiction.
+
+It's written in R Markdown, and compiled using Bookdown.
+
+[Here is a video on how to use RStudio and Git to edit the hackbook.]() NOT YET, SOON
+
+For minor contributions, you can create a Gitlab account and edit the R Markdown files directly. However, for major changes, you'll probably want to download R Studio and compile it to make sure everything works well.
+
+Versions are numbered using [semantic versioning](https://semver.org).
+
---
+++ build.sh
@@ -0,0 +1,107 @@
+#!/bin/bash
+
+# Install fonts
+cp Arial.ttf /usr/local/share/fonts/
+cp Dotum.ttf /usr/local/share/fonts/
+fc-cache -fv
+fc-list
+
+# Install tinytex
+Rscript -e "install.packages('tinytex')"
+Rscript -e "tinytex::install_tinytex(force = TRUE)"
+
+# Build EasyPeasy
+Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+
+# Build EasyPeasy translations
+buildpath=`pwd`
+langs="de it sv ru so ro sq nl pt-br pl ko"
+for lang in $langs
+do
+    mkdir -p _book/$lang
+    cd translations/$lang
+
+
+
+    ######################################################################
+    # This fixes a problem with Unicode characters in chapter/header
+    # names causing the pages to glitch. This was especially a problem
+    # in the Korean and Russian pages.
+    # What this does is first locally rewrite every markdown file,
+    # writing an ASCII-safe tagname to each header.
+    # Then, after the HTML files are generated, edit the HTML files with
+    # find and replace, changing every tagname back to proper sanitized
+    # Unicode tagnames.
+    ######################################################################
+    
+    backtick='`'
+    newline=$'\n'
+    headerid="0"
+    sedargs=""
+    
+    rmdfiles=`ls -1 | grep -iE '\.rmd'`
+    while IFS= read -r rmdfile
+    do
+        filecontent=`cat "$rmdfile"`
+        newfilecontent=""
+        while IFS= read -r fileline
+        do
+            lineisheader="0"
+            headerisnamed="0"
+            headerisunnumbered="0"
+            if [[ "${fileline:0:1}" == "#" ]]
+            then
+                if grep -Po "^((#)|(##))( |\t).*$" <<< "$fileline" &> /dev/null
+                then
+                    lineisheader="1"
+                    if grep -Po "^((#)|(##))( |\t).*{[ \t]{0,}#.*}[ \t]{0,}$" <<< "$fileline" &> /dev/null
+                    then
+                        headerisnamed="1"
+                    fi
+                    if grep -Po "^((#)|(##))( |\t).*{[ \t]{0,}-[ \t]{0,}[ \t]{0,}#.*}[ \t]{0,}$" <<< "$fileline" &> /dev/null
+                    then
+                        headerisnamed="1"
+                    fi
+                    if grep -Po "^((#)|(##))( |\t).*{[ \t]{0,}-[ \t]{0,}}[ \t]{0,}$" <<< "$fileline" &> /dev/null
+                    then
+                        headerisunnumbered="1"
+                    fi
+                fi
+            fi
+            if [ $lineisheader = 1 ] && [ $headerisnamed = 0 ]
+            then
+                headerunsignaled=`sed -e "s/{[ \t]\{0,\}-[ \t]\{0,\}}[ \t]\{0,\}$//g" <<< "$fileline"`
+                headername=`sed -e "s/^((#)|(##))//g" <<< "$headerunsignaled"`
+                sanitizedname=`sed -e "s/^\(\(#\)\|\(##\)\)//g" -e "s/[#\\\/?!.<>${backtick}',()&@%^$~{}+=;:\"|\*\x00-\x1F]//g" -e "s/\[//g" -e "s/]//g" -e "s/^[ \t]\+//g" -e "s/[ \t]\+$//g" -e "s/[ \t]\+/-/g" -e "s/[-]\+/-/g" -e "s/[[:upper:]]*/\L&/g" <<< "$headername"`
+                headerid=$((headerid+1))
+                if [ $headerisunnumbered = 0 ]
+                then
+                    newheader="$headerunsignaled {#headerplaceholder${headerid}id}"
+                else
+                    newheader="$headerunsignaled {-#headerplaceholder${headerid}id}"
+                fi
+                newfilecontent+="$newheader$newline"
+                sedargs+="-e s/headerplaceholder${headerid}id/${sanitizedname}/g "
+            else
+                newfilecontent+="$fileline$newline"
+            fi
+        done <<< "$filecontent"
+        echo "$newfilecontent" > "$rmdfile"
+    done <<< "$rmdfiles"
+
+    Rscript -e "bookdown::render_book('index.Rmd', 'all')"
+
+    shopt -s globstar
+    for file in _book/**
+    do
+        if [ -f "$file" ]
+        then
+            sed -i $sedargs "$file"
+            newfilename=`sed $sedargs <<< "$file"`
+            mv "$file" "$newfilename" &> /dev/null
+        fi
+    done
+
+    cp -r _book/* ../../_book/$lang/
+    cd $buildpath
+done
